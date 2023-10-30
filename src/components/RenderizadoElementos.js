5875dd30c1228b4c6c0bd7653fb4362016e29975
Javier Rodríguez Cortés <jrcortes02102005@educarex.es>
2023-10-30 12:54:08 +0000

    renderizado elementos

---
+++ src/components/RenderizadoElementos.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class RenderizadoElementos extends Component {
+    render(){
+        return(
+            <div>
+                <h2>Renderizado de elementos</h2>
+            </div>
+        )
+    }
+}
