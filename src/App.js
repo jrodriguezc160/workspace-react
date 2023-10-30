3c64d0b84828bdecb58fa95adbb335c3c427a812
Javier Rodríguez Cortés <jrcortes02102005@educarex.es>
2023-10-30 09:53:35 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ export default function App() {
     <div className="App">
       <h1>Hola React</h1>
       <h2>Mi primera toma de contacto con React desde un repositorio de Sandbox!!!</h2>
-      <br/>
-      <br/>
+      <br />
+      <br />
       <section>
-        <Componente msg="Hola, soy un componente funcional expresado desde una prop"/>
-        <Propiedades></Propiedades>
+        <Componente msg="Hola, soy un componente funcional expresado desde una prop" />
+        <Propiedades
+          cadena="Esto es una cadena de texto"
+          numero={19} boolean={true}
+          arreglo={[1, 2, 3]}
+          objeto={{ nombre: "Eduardo", apellido: "Camavinga" }}
+          funcion={num => num * num}
+          elementoReact={<i>Esto es un elemento React</i>}
+        />
       </section>
     </div>
   );
--- src/components/Propiedades.js
+++ src/components/Propiedades.js
@@ -3,7 +3,20 @@ import React from "react";
 export default function Propiedades(props) {
     return (
         <div>
-            <h2>Propiedades</h2>
+            <h2>{props.porDefecto}</h2>
+            <ul>
+                <li>{props.cadena}</li>
+                <li>{props.numero}</li>
+                <li>{props.boolean ? "Verdadero" : "Falso"}</li>
+                <li>{props.arreglo.join(", ")}</li>
+                <li>{props.objeto.nombre + " - " + props.objeto.apellido}</li>
+                <li>{props.arreglo.map(props.funcion).join(", ")}</li>
+                <li>{props.elementoReact}</li>
+            </ul>
         </div>
     )
+}
+
+Propiedades.defaultProps = {
+    porDefecto: "Las props"
 }
