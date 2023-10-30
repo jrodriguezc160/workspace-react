b5ceb7539a148357837970f57b3006f1dc0ac908
Javier Rodríguez Cortés <jrcortes02102005@educarex.es>
2023-10-30 09:51:05 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "./styles.css";
 import React from "react";
 import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hola React</h1>
       <h2>Mi primera toma de contacto con React desde un repositorio de Sandbox!!!</h2>
-      <br/>
-      <br/>
+      <br />
+      <br />
       <section>
-        <Componente msg="Hola, soy un componente funcional expresado desde una prop"/>
+        <Componente msg="Hola, soy un componente funcional expresado desde una prop" />
+        <Propiedades
+          cadena={"Esto es una cadena de texto"}
+          numero={19}
+          booleano={true}
+          arreglo={[1, 2, 3]}
+          objeto={{ nombre: "Cristiano", apellido: "Ronaldo" }}
+          funcion={num => num * num}
+          elementoReact={<i>Esto es un elemento React</i>}
+          componenteReact={<Componente msg="Soy un componente pasado como prop" />}
+        />
       </section>
     </div>
   );
