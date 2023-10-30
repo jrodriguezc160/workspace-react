6d4f8d9b1dbaad802af5247e608e66346fd1be49
Javier Rodríguez Cortés <jrcortes02102005@educarex.es>
2023-10-30 09:40:26 +0000

    Sandbox snapshot commit

--- src/components/Componente.js
+++ src/components/Componente.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
-function Componente(props) {
-    return <h2>{props.msg}</h2>
-}
+const Componente = (props) => <h2>{props.msg}</h2>
+
+// function Componente(props) {
+//     return <h2>{props.msg}</h2>
+// }
 
 // class Componente extends Component {
 //     render() {
