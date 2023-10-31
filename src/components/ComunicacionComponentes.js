8ab83d3b6bab8d51a314f9d41b3a5221b4f9592b
Javier Rodríguez Cortés <jrcortes02102005@educarex.es>
2023-10-31 09:14:59 +0000

    comcomponentes

---
+++ src/components/ComunicacionComponentes.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Padre extends Component {
+    render() {
+        return (
+            <>
+                <h2>Comunicación entre Componentes</h2>
+            </>
+        )
+    }
+}
